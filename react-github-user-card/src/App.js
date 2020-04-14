77debc4d19fb809c490a93a4bf7c4bf165bf4723
jaren <jarensmart@yahoo.com>
2020-04-14 02:47:06 -0400

    added most of the functionality

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      usersText: "",
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    // fetch(
+    //   `https://cors-anywhere.herokuapp.com/https://api.github.com/users/JarenSmart`
+    // );
+    //   .then((response) => response.json())
+    //   .then((person) => {
+    //     console.log("Person: ", person);
+    //     this.setState({ users: person.message });
+    //   })
+    //   .catch((error) => {
+    //     console.log("error: ", error);
+    //   });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+
+    //update the state
+    if (prevState.users !== this.state.doggos) {
+      console.log("Your users have loaded!");
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({ usersText: e.target.value });
+    console.log("this.state.usersText: ", this.state.usersText);
+  };
+
+  fetchUsers = (e) => {
+    e.preventDefault();
+
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.github.com/users/JarenSmart`
+    )
+      .then((response) => console.log(response))
+      .then((person) => {
+        console.log("Person: ", person);
+        this.setState({ users: person.message });
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Github User Card</h1>
+        <input
+          type="text"
+          value={this.state.usersText}
+          onChange={this.changeHandler}
+        />
+        <button onClick={this.fetchUsers}>Get new users!</button>
+        <div className="users">
+          {this.state.users.map((user) => (
+            <img width="200" src={user} alt={user} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
